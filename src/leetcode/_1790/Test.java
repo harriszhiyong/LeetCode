b4392d31d2c620c3c90d57ddcbc44c797fa2dccc
张智勇 <harris@MacPro.local>
2021-06-30 20:25:55 +0800

    tesst

--- src/leetcode/_1790/Test.java
+++ src/leetcode/_1790/Test.java
@@ -3,6 +3,6 @@ package leetcode._1790;
 public class Test {
     public static void main(String[] args) {
         System.out.println(new Solution().areAlmostEqual("a", "b"));
-        //test
+        //test.
     }
 }
